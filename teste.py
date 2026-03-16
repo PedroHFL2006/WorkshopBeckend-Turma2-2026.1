0760e22a4f7f3c7919b4c52b42a0714fc67e3a7f
PedroHFL2006 <phfigueiredolima@gmail.com>
2026-03-16 17:03:09 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,11 +1,11 @@
 #Média Ponderada
 
-a = int(input("Escolha o numero a:"))
-b = int(input("Escolha o numero b:"))
-c = int(input("Escolha o numero c:"))
-peso_a = int(input("Escolha o peso a:"))
-peso_b = int(input("Escolha o peso b:"))
-peso_c = int(input("Escolha o peso c:"))
+a = int(input("Escolha o numero A:"))
+b = int(input("Escolha o numero B:"))
+c = int(input("Escolha o numero C:"))
+peso_a = int(input("Escolha o peso A:"))
+peso_b = int(input("Escolha o peso B:"))
+peso_c = int(input("Escolha o peso C:"))
 
 media_ponderado = (a * peso_a) + (b * peso_b) + (c * peso_c) / (peso_a + peso_b + peso_c)
 print(int(media_ponderado))
