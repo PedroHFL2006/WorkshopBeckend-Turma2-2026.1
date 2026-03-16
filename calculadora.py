2fc4c03ca17197b8e26ed4af477317ea3ea8f9f6
PedroHFL2006 <phfigueiredolima@gmail.com>
2026-03-16 17:40:10 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,33 @@
+# Source - https://stackoverflow.com/a/69851000
+# Posted by ItsMe, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-03-16, License - CC BY-SA 4.0
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def div(num1, num2):
+    return num1/num2
+
+def multi(num1, num2):
+    return num1*num2
+
+def main():
+    operation = input("Qual a formula que deseja?(+, -, *, or /):")
+    if (operation != "+" and operation != "-" and operation != "*" and operation != "/"):
+        print("Your input is invalid. Please enter a valid input.")
+    else:
+        num1 = float(input("Enter value for num1: "))
+        num2 = float(input("Enter value for num2: "))
+        if (operation == "+"):
+            print(add(num1, num2))
+        elif (operation == "-"):
+            print(subtract(num1, num2))
+        elif (operation == "*"):
+            print(multi(num1,num2))
+        elif (operation == "/"):
+            print(div(num1,num2))
+
+main() # Added main outside the function
