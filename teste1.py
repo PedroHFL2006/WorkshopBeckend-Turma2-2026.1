8e1ed3b23480a98a7a2a2fc41e279c0cf38e1fe4
PedroHFL2006 <phfigueiredolima@gmail.com>
2026-03-17 16:24:25 -0300

    Organização

--- teste1.py
+++ teste1.py
@@ -1,12 +1,11 @@
-#Média Ponderada
+# Calcular Média Ponderada
 
 a = int(input("Escolha o numero A:"))
 b = int(input("Escolha o numero B:"))
 c = int(input("Escolha o numero C:"))
 peso_a = int(input("Escolha o peso A:"))
 peso_b = int(input("Escolha o peso B:"))
-peso_c = int(input("Escolha o peso C:"))
+peso_c = int(input("Escolha o peso C:")) 
 
 media_ponderado = (a * peso_a) + (b * peso_b) + (c * peso_c) / (peso_a + peso_b + peso_c)
-print(int(media_ponderado))
-
+print(int(media_ponderado)) 
