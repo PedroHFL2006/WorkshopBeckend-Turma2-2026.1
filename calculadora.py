e756347c97eed66207318f53151cceff46093050
PedroHFL2006 <phfigueiredolima@gmail.com>
2026-03-17 14:15:15 -0300

    Calculadora Python Atualizada
    -Código revisado
    -Adição da Potencia (**)
    -Adição do Histórico de Cálculos

--- calculadora.py
+++ calculadora.py
@@ -1,29 +1,45 @@
+def salvar_historico(texto):
+  with open("historico.txt", "a") as arquivo:
+    arquivo.write(texto + "\n")
+
 def adi(num1, num2):
     return num1 + num2
-    
+
 def subt(num1, num2):
     return num1 - num2
-
-def div(num1, num2):
-    return num1 / num2
 
 def multi(num1, num2):
     return num1 * num2
 
+def divi(num1, num2):
+    return num1 / num2
+
+def expo(num1, num2):
+    return num1 ** num2
+
 def main():
-    operacao = input("Qual a formula que deseja?(+, -, *, ou /):")
-    if (operacao != "+" and operacao != "-" and operacao != "*" and operacao != "/"):
-        print("Valor invalido escolhido. Porfavor escolha outro valor.")
-    else:
-        num1 = float(input("Coloque o valor num1: "))
-        num2 = float(input("Coloque o valor num2: "))
-        if (operacao == "+"):
-            print(adi(num1, num2))
-        elif (operacao == "-"):
-            print(subt(num1, num2))
-        elif (operacao == "*"):
-            print(multi(num1, num2))
-        elif (operacao == "/"):
-            print(div(num1, num2))
+  operacao = input("Qual a operação desejada? (+, -, *, /, **):")
+
+  if operacao not in ["+", "-", "*", "/", "**"]: # "!="
+    print("Valor Inválido")
+  else:
+    num1 = float(input("Coloque o valor num1: "))
+    num2 = float(input("Coloque o valor num2: "))
+
+    if operacao == "+":
+      resultado = adi(num1, num2)
+    elif operacao == "-":
+      resultado = subt(num1, num2)
+    elif operacao == "*":
+      resultado = multi(num1, num2)
+    elif operacao == "/":
+      resultado = divi(num1, num2)
+    elif operacao == "**":
+      resultado = expo(num1, num2)
+
+    print(f"Resultado: {resultado}")
+
+    linha_historico = f"{num1} {operacao} {num2} = {resultado}"
+    salvar_historico(linha_historico)
 
 main()
